7a503cdfb0de89c44013f644bbf198c38e495030
Yurii Gorb <G.Y.Andreevich@gmail.com>
2019-09-23 11:43:25 +0300

    v1.3.2

--- src/com/hillel/howmework/Main.java
+++ src/com/hillel/howmework/Main.java
@@ -2,12 +2,14 @@ package com.hillel.howmework;
 
 public class Main {
     public static void main(String[] args) {
-
+        //Тут можно выстовить до 100 цикл.
         for(int i = 1; i <= 10; i++){
             Person firstPerson = new Man();
             Person secondPerson = new Woman();
-            System.out.println(firstPerson.toString() + "\n=============================");
-            System.out.println(secondPerson.toString() + "\n=============================");
+            System.out.println(i + ". " + firstPerson.toString()
+                    + "\n-------------------------------------------");
+            System.out.println(i + ". " + secondPerson.toString()
+                    + "\n===========================================");
         }
     }
 }
--- src/com/hillel/howmework/Man.java
+++ src/com/hillel/howmework/Man.java
@@ -2,11 +2,11 @@ package com.hillel.howmework;
 
 
 public class Man extends Person {
-    protected static String[] firstName = {"John", "Marcus", "Susan", "Henry", "Garry", "Ron", "Barrie", "Fabian"};
+    protected static String[] firstName = {"John", "Marcus", "Susan", "Henry", "Harry", "Ron", "Barrie", "Fabian"};
     protected static String[] lastName = {"Smith", "Johnson", "Brown", "Aly", "Davis", "Potter", "Wilson", "Black"};
     protected static int[] age = {20, 40};
     protected static int[] height = {165, 200};
-    protected static int[] weight = {60, 75, 80, 100};
+    protected static int[] weight = {60, 81, 80, 100};
 
     public Man() {
         super(firstName, lastName, age, height, weight);
